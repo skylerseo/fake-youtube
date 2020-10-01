d5bb57643e852e2faaae917d8d2b6457d995eb01
Skyler <skylerseo.dev@gmail.com>
2020-10-01 16:29:33 +0900

    Updates

---
+++ precache-manifest.6550fa4d5922e4711d6e210c245b0b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401dead19e46d39af3dc6ba07238da9d",
+    "url": "/fake-youtube/index.html"
+  },
+  {
+    "revision": "29fd0bcb52fd04e3ba57",
+    "url": "/fake-youtube/static/css/main.6358ba25.chunk.css"
+  },
+  {
+    "revision": "293f2d0234843f59bdf0",
+    "url": "/fake-youtube/static/js/2.59b0d8aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fake-youtube/static/js/2.59b0d8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29fd0bcb52fd04e3ba57",
+    "url": "/fake-youtube/static/js/main.d5f23a50.chunk.js"
+  },
+  {
+    "revision": "5e1b88fe53f4213d195d",
+    "url": "/fake-youtube/static/js/runtime-main.b363be39.js"
+  }
+]);
